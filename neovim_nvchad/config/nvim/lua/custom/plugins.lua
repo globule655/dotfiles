7c4ca013eb089593fac31185aafb285079f186c0
unknown <globule655@gmail.com>
2023-10-22 00:25:24 +0200

    added nvim-surround plugin

--- neovim_nvchad/config/nvim/lua/custom/plugins.lua
+++ neovim_nvchad/config/nvim/lua/custom/plugins.lua
@@ -67,6 +67,16 @@ local plugins = {
     dependencies = { "nvim-lua/plenary.nvim" },
   },
 
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
