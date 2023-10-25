a6189b82acffa744034bc64557fef25059e883ad
Guillaume Debros <guillaume.debros@digeiz.com>
2023-10-25 10:57:15 +0200

    oil.nvim configuration

--- neovim_nvchad/config/nvim/lua/custom/plugins.lua
+++ neovim_nvchad/config/nvim/lua/custom/plugins.lua
@@ -57,7 +57,12 @@ local plugins = {
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("oil").setup()
+      require("oil").setup({
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = true,
+        },
+      })
     end,
     lazy = false,
   },
