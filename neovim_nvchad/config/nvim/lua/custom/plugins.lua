c8b043083b0555fd8d45655f3b7131487d4b7aee
globule <globule655@gmail.com>
2023-10-28 00:35:55 +0200

    added setup for git-worktree plugin

--- neovim_nvchad/config/nvim/lua/custom/plugins.lua
+++ neovim_nvchad/config/nvim/lua/custom/plugins.lua
@@ -90,6 +90,7 @@ local plugins = {
   {
     "ThePrimeagen/git-worktree.nvim",
     config = function()
+      require("git-worktree").setup({})
       require("telescope").load_extension("git_worktree")
     end,
   },
