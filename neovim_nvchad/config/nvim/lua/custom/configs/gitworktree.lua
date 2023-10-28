d938787a6bc61775303358849f929db01234b459
globule <globule655@gmail.com>
2023-10-28 18:54:11 +0200

    ""

--- neovim_nvchad/config/nvim/lua/custom/configs/gitworktree.lua
+++ neovim_nvchad/config/nvim/lua/custom/configs/gitworktree.lua
@@ -1,9 +1,9 @@
-vim.keymap.set("n", "<leader>gw", function ()
+vim.keymap.set("n", "<leader>gws", function ()
   require('telescope').extensions.git_worktree.git_worktrees()
 end,
   { desc = "Telescope switch & delete worktrees"})
 
-vim.keymap.set("n", "<leader>gc", function ()
+vim.keymap.set("n", "<leader>gwc", function ()
   require('telescope').extensions.git_worktree.create_git_worktree()
 end,
   { desc = "Telescope create worktree"})
--- neovim_nvchad/config/nvim/lua/custom/init.lua
+++ neovim_nvchad/config/nvim/lua/custom/init.lua
@@ -2,4 +2,5 @@ require "custom.configs.oilnvim"
 require "custom.configs.filetypes"
 require "custom.configs.vimopts"
 require "custom.configs.harpoon"
-require("custom.configs.git-worktree")
+require("custom.configs.gitworktree")
+require("custom.configs.neogit")
