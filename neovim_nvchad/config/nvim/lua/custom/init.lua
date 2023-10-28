80950b9f0acd138cc1865c1e14b25cde2c448482
globule <globule655@gmail.com>
2023-10-28 19:11:48 +0200

    "harmonized syntax"

--- neovim_nvchad/config/nvim/lua/custom/init.lua
+++ neovim_nvchad/config/nvim/lua/custom/init.lua
@@ -1,6 +1,6 @@
-require "custom.configs.oilnvim"
-require "custom.configs.filetypes"
-require "custom.configs.vimopts"
-require "custom.configs.harpoon"
+require("custom.configs.oilnvim")
+require("custom.configs.filetypes")
+require("custom.configs.vimopts")
+require("custom.configs.harpoon")
 require("custom.configs.gitworktree")
 require("custom.configs.neogit")
