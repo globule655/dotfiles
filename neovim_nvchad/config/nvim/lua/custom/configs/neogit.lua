20ef6386451e056fdc15e2dd36e153625fc33d37
globule <globule655@gmail.com>
2023-10-29 15:50:14 +0100

    "removed trailing spaces"

--- neovim_nvchad/config/nvim/lua/custom/configs/neogit.lua
+++ neovim_nvchad/config/nvim/lua/custom/configs/neogit.lua
@@ -1,15 +1,15 @@
 vim.keymap.set("n", "<leader>go", function ()
   require("neogit").open({ cwd = "%:p:h" })
-end, 
+end,
   { desc = "Open neogit"})
 
 vim.keymap.set("n", "<leader>gc", function ()
   require("neogit").open({ "commit", cwd = "%:p:h" })
-end, 
+end,
   { desc = "Open neogit commit popup"})
 
 vim.keymap.set("n", "<leader>gs", function ()
   require("neogit").open({ kind = "split", cwd = "%:p:h" })
-end, 
+end,
   { desc = "Open neogit with kind split"})
 
