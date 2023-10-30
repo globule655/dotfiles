f5e3c570a9b7a87cdfe5ef7bf51660d04b337675
globule <globule655@gmail.com>
2023-10-30 20:31:35 +0100

    "Mappings for git-worktree without telescope"

--- neovim_nvchad/config/nvim/lua/custom/configs/gitworktree.lua
+++ neovim_nvchad/config/nvim/lua/custom/configs/gitworktree.lua
@@ -1,11 +1,20 @@
-vim.keymap.set("n", "<leader>gws", function ()
-  require('telescope').extensions.git_worktree.git_worktrees()
-end,
-  { desc = "Telescope switch & delete worktrees"})
-
 vim.keymap.set("n", "<leader>gwc", function ()
-  require('telescope').extensions.git_worktree.create_git_worktree()
+  local path = vim.fn.input("Path: ")
+  local branch = vim.fn.input("Branch: ")
+  local upstream = vim.fn.input("upstream: ")
+  require('git-worktree').create_worktree(path, branch, upstream)
 end,
-  { desc = "Telescope create worktree"})
+  { desc = "Create new worktree"})
 
+vim.keymap.set("n", "<leader>gws", function ()
+  local path = vim.fn.input("Path: ")
+  require('git-worktree').switch_worktree(path)
+end,
+  { desc = "Switch worktree"})
+
+vim.keymap.set("n", "<leader>gwd", function ()
+  local path = vim.fn.input("Path: ")
+  require('git-worktree').delete_worktree(path)
+end,
+  { desc = "Delete worktree"})
 
