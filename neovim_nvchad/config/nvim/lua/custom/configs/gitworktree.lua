972e49064de36be719959edb9002f04adb8a84e3
Guillaume Debros <guillaume.debros@digeiz.com>
2023-11-03 12:18:51 +0100

    "Fixed telescope config to work with git-worktree"

--- neovim_nvchad/config/nvim/lua/custom/configs/gitworktree.lua
+++ neovim_nvchad/config/nvim/lua/custom/configs/gitworktree.lua
@@ -1,20 +1,10 @@
 vim.keymap.set("n", "<leader>gwc", function ()
-  local path = vim.fn.input("Path: ")
-  local branch = vim.fn.input("Branch: ")
-  local upstream = vim.fn.input("upstream: ")
-  require('git-worktree').create_worktree(path, branch, upstream)
+    require('telescope').extensions.git_worktree.create_git_worktree()
 end,
   { desc = "Create new worktree"})
 
 vim.keymap.set("n", "<leader>gws", function ()
-  local path = vim.fn.input("Path: ")
-  require('git-worktree').switch_worktree(path)
+  require('telescope').extensions.git_worktree.git_worktrees()
 end,
   { desc = "Switch worktree"})
-
-vim.keymap.set("n", "<leader>gwd", function ()
-  local path = vim.fn.input("Path: ")
-  require('git-worktree').delete_worktree(path)
-end,
-  { desc = "Delete worktree"})
 
