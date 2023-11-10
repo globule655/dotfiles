9c4393b6710def45bde700bd533244bfb9a947d6
Guillaume Debros <guillaume.debros@digeiz.com>
2023-11-10 09:46:26 +0100

    "Added telescope and codeium mappings"

--- nvim/lua/options/mappings/telescope.lua
+++ nvim/lua/options/mappings/telescope.lua
@@ -4,3 +4,5 @@ vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope search buffers"})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Telescope Open old files"})
 vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, { desc = "Telescope search current buffer"})
+vim.keymap.set('n', '<leader>th', '<CMD>Telescope themes<CR>', { desc = "Telescope themes"})
+vim.keymap.set('n', '<leader>ma', '<CMD>Telescope marks<CR>', { desc = "Telescope marks"})
