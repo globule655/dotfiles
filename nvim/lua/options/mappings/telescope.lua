dd75fac49cd4ffc87e16f3421bf71db8b22dc034
Guillaume Debros <guillaume.debros@digeiz.com>
2023-11-10 09:51:40 +0100

    "Added git mappings to Telescope"

--- nvim/lua/options/mappings/telescope.lua
+++ nvim/lua/options/mappings/telescope.lua
@@ -6,3 +6,6 @@ vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Telescope Open old
 vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, { desc = "Telescope search current buffer"})
 vim.keymap.set('n', '<leader>th', '<CMD>Telescope themes<CR>', { desc = "Telescope themes"})
 vim.keymap.set('n', '<leader>ma', '<CMD>Telescope marks<CR>', { desc = "Telescope marks"})
+vim.keymap.set('n', '<leader>cm', '<CMD>Telescope git_commits<CR>', { desc = "Telescope git_commits"})
+vim.keymap.set('n', '<leader>gt', '<CMD>Telescope git_status<CR>', { desc = "Telescope git_status"})
+vim.keymap.set('n', '<leader>gb', '<CMD>Telescope git_branches<CR>', { desc = "Telescope git_branches"})
