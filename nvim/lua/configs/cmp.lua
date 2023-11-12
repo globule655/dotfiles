ab95f9aa4a6e597fd10249d20a6ebb4b2caf2cc2
globule <globule655@gmail.com>
2023-11-12 17:24:52 +0100

    "Remove trailing whitespace in cmp menu"

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -27,7 +27,7 @@ local options = {
     format = function(entry, vim_item)
       local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
       local strings = vim.split(kind.kind, "%s", { trimempty = true })
-      kind.kind = " " .. (strings[1] or "") .. " "
+      kind.kind = "" .. (strings[1] or "") .. ""
       kind.menu = "    (" .. (strings[2] or "") .. ")"
       return kind
     end,
