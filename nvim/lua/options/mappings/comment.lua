2fc6f39283cad13395efbf0470a211d897ea32b6
guillaume <globule655@gmail.com>
2023-11-18 18:13:38 +0100

    fixed comment mappings

--- nvim/lua/options/mappings/comment.lua
+++ nvim/lua/options/mappings/comment.lua
@@ -1,6 +1,6 @@
 vim.keymap.set('v', '<leader>/', "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", { desc = "Telescope find files"})
 
-vim.keymap.set('v', '<leader>/', function()
+vim.keymap.set('n', '<leader>/', function()
   require("Comment.api").toggle.linewise.current()
 end,
   { desc = "Toggle comment" })
