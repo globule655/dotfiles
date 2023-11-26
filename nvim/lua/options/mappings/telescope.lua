485dd6a0fbfcc900ae63f2a8190a98e1430f3a89
Guillaume Debros <guillaume.debros@digeiz.com>
2023-11-26 20:58:21 +0100

    "Added zellij config files"

--- nvim/lua/options/mappings/telescope.lua
+++ nvim/lua/options/mappings/telescope.lua
@@ -4,6 +4,7 @@ vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope search buffers"})
 vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = "Telescope Open old files"})
 vim.keymap.set('n', '<leader>fz', builtin.current_buffer_fuzzy_find, { desc = "Telescope search current buffer"})
+vim.keymap.set('n', '<leader>fm', builtin.marks, { desc = "Telescope list marks"})
 vim.keymap.set('n', '<leader>ma', '<CMD>Telescope marks<CR>', { desc = "Telescope marks"})
 vim.keymap.set('n', '<leader>cm', '<CMD>Telescope git_commits<CR>', { desc = "Telescope git_commits"})
 vim.keymap.set('n', '<leader>gt', '<CMD>Telescope git_status<CR>', { desc = "Telescope git_status"})
