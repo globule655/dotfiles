6b8095d59890a80ea2e9b7305587d651b8ac13c2
Guillaume Debros <guillaume.debros@digeiz.com>
2024-07-13 21:38:26 +0200

    "Update keybindings for codesnap"

--- nvim/lua/options/mappings/codesnap.lua
+++ nvim/lua/options/mappings/codesnap.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('x', '<leader>cc', '<cmd>CodeSnap<cr>', { desc = 'Save selected code snapshot into clipboard' })
-vim.keymap.set('x', '<leader>cs', '<cmd>CodeSnapSave<cr>', { desc = 'Save selected code snapshot in ~/Pictures' })
+vim.keymap.set('x', '<leader>cc', '<cmd>CodeSnap<CR>', { desc = 'Save selected code snapshot into clipboard' })
+vim.keymap.set('x', '<leader>cs', '<cmd>CodeSnapSave<CR>', { desc = 'Save selected code snapshot in ~/Pictures' })
