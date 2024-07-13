2f800e871fb2645727c75b1a3e8712a7bc8e6f7d
Guillaume Debros <guillaume.debros@digeiz.com>
2024-07-13 21:12:33 +0200

    "Added missing codesnap keybindings"

---
+++ nvim/lua/options/mappings/codesnap.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('x', '<leader>cc', '<cmd>CodeSnap<cr>', { desc = 'Save selected code snapshot into clipboard' })
+vim.keymap.set('x', '<leader>cs', '<cmd>CodeSnapSave<cr>', { desc = 'Save selected code snapshot in ~/Pictures' })
