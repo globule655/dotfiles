1339acb6464aa20e61bd58789e30909b34f72950
Guillaume Debros <guillaume.debros@digeiz.com>
2024-07-14 12:38:34 +0200

    "Obsidian integration config"

--- nvim/lua/configs/obsidian.lua
+++ nvim/lua/configs/obsidian.lua
@@ -6,6 +6,75 @@ local obsidian_opts = {
     },
   },
 
+  -- Optional, customize how note IDs are generated given an optional title.
+  ---@param title string|?
+  ---@return string
+  note_id_func = function(title)
+    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- In this case a note with the title 'My new note' will be given an ID that looks
+    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    local suffix = ""
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    return tostring(os.time()) .. "-" .. suffix
+  end,
+
+  -- Optional, customize how note file names are generated given the ID, target directory, and title.
+  ---@param spec { id: string, dir: obsidian.Path, title: string|? }
+  ---@return string|obsidian.Path The full path to the new note.
+  note_path_func = function(spec)
+    -- This is equivalent to the default behavior.
+    local path = spec.dir / tostring(spec.id)
+    return path:with_suffix(".md")
+  end,
+
+  note_subdir = "inbox",
+  new_notes_location = "notes_subdir",
+
+  completion = {
+    nvim_cmp = true,
+  },
+
+  -- Optional, boolean or a function that takes a filename and returns a boolean.
+  -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
+  disable_frontmatter = false,
+
+  -- Optional, alternatively you can customize the frontmatter data.
+  ---@return table
+  note_frontmatter_func = function(note)
+    -- Add the title of the note as an alias.
+    if note.title then
+      note:add_alias(note.title)
+    end
+
+    local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+    -- `note.metadata` contains any manually added fields in the frontmatter.
+    -- So here we just make sure those fields are kept in the frontmatter.
+    if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+      for k, v in pairs(note.metadata) do
+        out[k] = v
+      end
+    end
+
+    return out
+  end,
+
+  templates = {
+    folder = "Templates",
+    date_format = "%Y-%m-%d",
+    time_format = "%H:%M",
+    -- A map for custom variables, the key should be the variable and the value a function
+    substitutions = {},
+  },
+
   markdown_link_func = function(opts)
     return require("obsidian.util").markdown_link(opts)
   end,
---
+++ nvim/lua/options/mappings/obsidian.lua
@@ -0,0 +1,1 @@
+vim.keymap.set({ 'n', 'v' }, '<leader>on', '<CMD>ObsidianNewFromTemplate note<CR>' )
