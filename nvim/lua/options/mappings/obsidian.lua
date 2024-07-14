4f10f0bc3fea0c577df91b4d94b8dec5a6f910bb
Guillaume Debros <guillaume.debros@digeiz.com>
2024-07-14 19:58:57 +0200

    "Added backlink keymap for obsidian"

--- nvim/lua/options/mappings/obsidian.lua
+++ nvim/lua/options/mappings/obsidian.lua
@@ -1,2 +1,3 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>on', ':ObsidianTemplate note<CR>', { desc = 'Obsidian reformat file from note template'})
 vim.keymap.set({ 'n', 'v' }, '<leader>ol', ':ObsidianLinkNew<CR>', { desc = 'Create New Link from selected text'})
+vim.keymap.set({ 'n', 'v' }, '<leader>obl', ':ObsidianBackLink<CR>', { desc = 'Open Obsidian Backlink in Telescope'})
