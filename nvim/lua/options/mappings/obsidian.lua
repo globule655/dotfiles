be58e2bc22d28a9925580d03996ffdb85e1e8185
Guillaume Debros <guillaume.debros@digeiz.com>
2024-07-14 16:33:39 +0200

    "Added Obsidian keymap to create links"

--- nvim/lua/options/mappings/obsidian.lua
+++ nvim/lua/options/mappings/obsidian.lua
@@ -1,1 +1,2 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>on', ':ObsidianTemplate note<CR>', { desc = 'Obsidian reformat file from note template'})
+vim.keymap.set({ 'n', 'v' }, '<leader>ol', ':ObsidianLinkNew<CR>', { desc = 'Create New Link from selected text'})
