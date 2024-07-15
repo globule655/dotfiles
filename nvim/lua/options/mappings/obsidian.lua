7235d07e6c34dcc42402f8e28a99cd288362ab7d
Guillaume Debros <guillaume.debros@digeiz.com>
2024-07-15 09:20:54 +0200

    "Obsidian mappings"

--- nvim/lua/options/mappings/obsidian.lua
+++ nvim/lua/options/mappings/obsidian.lua
@@ -1,3 +1,5 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>on', ':ObsidianTemplate note<CR>', { desc = 'Obsidian reformat file from note template'})
+vim.keymap.set({ 'n', 'v' }, '<leader>ot', ':ObsidianTemplate<CR>', { desc = 'Open Telescope to select template to reformat file from'})
+vim.keymap.set({ 'n', 'v' }, '<leader>of', ':s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<CR>', { desc = 'Obsidian reformat file from note template'})
 vim.keymap.set({ 'n', 'v' }, '<leader>ol', ':ObsidianLinkNew<CR>', { desc = 'Create New Link from selected text'})
-vim.keymap.set({ 'n', 'v' }, '<leader>obl', ':ObsidianBackLink<CR>', { desc = 'Open Obsidian Backlink in Telescope'})
+vim.keymap.set({ 'n', 'v' }, '<leader>bl', ':ObsidianBackLink<CR>', { desc = 'Open Obsidian Backlink in Telescope'})
