8293ca578b1245f225d111acf0ebbd2f11e4d90f
Guillaume Debros <guillaume.debros@digeiz.com>
2024-07-23 16:45:55 +0200

    "Added ansible language server"

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -400,7 +400,13 @@ local plugs = {
     "folke/flash.nvim",
     event = "VeryLazy",
     ---@type Flash.Config
-    opts = {},
+    opts = {
+      modes = {
+        char = {
+          enabled = false,
+        }
+      }
+    },
     -- stylua: ignore
     keys = {
       { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
