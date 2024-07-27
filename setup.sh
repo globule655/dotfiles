62b8d6ad2a2d6cb5943a81f22d321d1170f43e09
Guillaume Debros <guillaume.debros@digeiz.com>
2024-07-27 17:49:39 +0200

    "Script for initial installation"

--- setup.sh
+++ setup.sh
@@ -3,15 +3,35 @@
 # Install all needed packages and setup environment
 # detect os distribution
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+NERD_FONT_NAME=JetBrainsMono
+NERD_FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/${NERD_FONT_NAME}.zip"
 
 pkgs=(
   make
   cmake
   gcc
   git
+  tmux
+  kitty
   unzip
   eza
   zsh
+  zoxide
+  sway
+  )
+
+zsh_plugs=(
+  https://github.com/zsh-users/zsh-autosuggestions.git
+  https://github.com/zsh-users/zsh-syntax-highlighting.git
+  )
+
+docker_pkgs=(
+  docker-ce
+  docker-ce-cli
+  containerd.io
+  docker-compose-plugin
+  docker-buildx-plugin
   )
 
 get_latest_release () {
@@ -22,6 +42,14 @@ get_latest_release () {
 
 ZELLIJ_VERSION=$(get_latest_release zellij-org/zellij)
 
+clone_plugs () {
+  mkdir -p $HOME/.zsh && cd $_
+  for repos in $zsh_plugs; do
+    git clone repos
+  done
+  cd $HOME
+}
+
 find_os () {
   if [ 'ID_LIKE' in /etc/os-release ]; then
     os=$(cat /etc/os-release |grep "ID_LIKE" | cut -d'=' -f2)
@@ -31,14 +59,31 @@ find_os () {
   echo $os
 }
 
+create_symlinks () {
+  cd $SCRIPT_DIR
+  mkdir -p $HOME/.config
+  for file in $(find $SCRIPT_DIR -maxdepth 1 -type d -not -path '*.git' | awk -F/ '{print $2}'); do
+    ln -sf $SCRIPT_DIR/$file $HOME/.config/$file
+  done
+  cd $HOME
+}
+
+install_fonts () {
+  wget -qO- $NERD_FONT_URL | unzip -j $NERD_FONT_NAME.zip -d $HOME/local/share/fonts
+}
+
 third_parties () {
   case "$(find_os)" in
     "debian") wget -O /tmp/zellij.tar.gz https://github.com/zellij-org/zellij/releases/download/$ZELLIJ_VERSION/zellij-x86_64-unknown-linux-musl.tar.gz \
       && tar -xvzf /tmp/zellij.tar.gz -C /usr/bin && chmod 755 /usr/bin/zellij \
-      && curl -sS https://starship.rs/install.sh | sh -s -- -y
+      && curl -sS https://starship.rs/install.sh | sh -s -- -y \
+      && curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh \
+      && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
       ;;
     "fedora") dnf copr enable varlad/zellij && dnf install -y zellij \
-      && curl -sS https://starship.rs/install.sh | sh -s -- -y
+      && curl -sS https://starship.rs/install.sh | sh -s -- -y \
+      && curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh \
+      && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
       ;;
     *) echo 'system not supported' && exit 1
       ;;
@@ -52,12 +97,21 @@ package_install () {
       && wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg \
       && echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list \
       && chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list \
-      && apt update && apt install -y $1
+      && apt update && apt install -y $pkgs
       ;;
-    "fedora") dnf install -y $1
+    "fedora") dnf install -y dnf-plugins-core \
+      && dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo \
+      && dnf install -y $pkgs
+      && dnf install -y $docker_pkgs
       ;;
     *) echo 'system not supported' && exit 1
       ;;
   esac
 }
 
+
+clone_plugs
+create_symlinks
+package_install
+install_fonts
+third_parties
