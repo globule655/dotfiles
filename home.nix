aabad55bc112bfe13c6bab95af6fed8cad7b5699
Guillaume Debros <guillaume.debros@digeiz.com>
2024-08-07 13:24:48 +0200

    "Added clock to tmux config"

--- home.nix
+++ home.nix
@@ -184,6 +184,7 @@ in
       set -g @catppuccin_status_right_separator_inverse "yes"
       set -g @catppuccin_status_fill "all"
       set -g @catppuccin_status_connect_separator "no"
+      set -g status-right '#[fg=black]%Y-%m-%d %H:%M#[default]'
     '';
     };
   };
