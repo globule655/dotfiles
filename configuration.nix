03e3d93c852d1d0638df84d0bcf11ef35d57bedc
Globule <globule655@gmail.com>
2024-08-12 11:15:16 +0200

    "Remove comments"

--- configuration.nix
+++ configuration.nix
@@ -159,8 +159,6 @@ fonts = {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
     curl
     file
     git
