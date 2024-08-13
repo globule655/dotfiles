3c4ca513467716a1f9619e4e0a226a1a16ec1c39
gdebros <guillaume.debros@digeiz.com>
2024-08-13 17:59:46 +0200

    "Test ipsec config"

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -2,9 +2,11 @@
 
   imports = [
     ./kanata.nix
+    ./strongswan.nix
   ];
 
   config.kanata-service.enable = lib.mkDefault true;
+  config.strongswan-service.enable = lib.mkDefault false;
 
 }
 
---
+++ modules/services/strongswan.nix
@@ -0,0 +1,51 @@
+{inputs, outputs, lib, config, pkgs, ... }: {
+
+  options = {
+    strongswan-service = {
+      enable = lib.mkEnableOption "Enable strongswan service";
+    };
+  };
+
+  config = lib.mkIf config.strongswan-service.enable {
+    services.strongswan = {
+      enable = true;
+      connections = {
+        setup = {
+          uniqueids = "yes";
+        };
+        default = {
+          fragmentation = "yes";
+          keyexchange = "ikev2";
+          reauth = "no";
+          forceencaps = "yes";
+          compress = "no";
+          mobike = "no";
+          rekey = "yes";
+          installpolicy = "yes";
+          type = "tunnel";
+          dpdaction = "restart";
+          dpddelay = "90s";
+          dpdtimeout = "540s";
+          closeaction = "restart";
+          margintime = "10m";
+          auto = "add";
+        };
+        home_remote = {
+          also = "default";
+          ikelifetime = "8400s";
+          lifetime = "3600s";
+          leftauth = "eap-tls";
+          leftcert = "/etc/ipsec.d/certs/globule.crt";
+          right = "78.193.216.210";
+          rightid = "82.67.241.60";
+          rightauth = "pubkey";
+          rightca = "/CN=ipsec-CA/C=FR/ST=IDF/L=Paris/O=firehack/OU=firehack";
+          rightsubnet = "10.30.0.0/28,10.29.100.0/28";
+          eap_identity = "%identity";
+        };
+      };
+    };
+  };
+
+}
+
