501679c4a6022c6c632084dfccbdcd694b0a509e
Globule <globule655@gmail.com>
2024-08-24 21:27:04 +0200

    "Noice show macro recording in lualine"

--- nvim/lua/configs/lualine.lua
+++ nvim/lua/configs/lualine.lua
@@ -2,6 +2,15 @@ local lualine = {
   options = {
     theme = "auto",
   },
+  sections = {
+    lualine_x = {
+      {
+        require("noice").api.statusline.mode.get,
+        cond = require("noice").api.statusline.mode.has,
+        color = { fg = "#ff9e64" },
+      }
+    },
+  },
 }
 
 return lualine
