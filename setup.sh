7ca7e3488c3270d7a809e44f28819ad32b916cc4
gdebros <guillaume.debros@digeiz.com>
2024-10-14 09:43:55 +0200

    "Updated setup script"

--- setup.sh
+++ setup.sh
@@ -19,6 +19,8 @@ ln -sf $HOME/.dotfiles $HOME/.config/home-manager
 # Backup bashrc and bash_profile files
 mv $HOME/.bashrc{,.bkp}
 mv $HOME/.bash_profile{,.bkp}
+# Allows sway to load nix installed binaries
+echo "$HOME/.nix-profile/bin:$PATH" > $HOME/.config/environment.d/sway.conf
 
 # Install home-manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
