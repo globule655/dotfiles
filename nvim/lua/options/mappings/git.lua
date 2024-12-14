8ddda3c93c933febf1d5b78ce7d8deccb0f68f4e
gdebros <guillaume.debros@digeiz.com>
2024-12-14 17:15:34 +0100

    Changed mapping for git blame

--- nvim/lua/options/mappings/git.lua
+++ nvim/lua/options/mappings/git.lua
@@ -1,2 +1,2 @@
 local gitsigns = require("gitsigns")
-vim.keymap.set('n', '<leader>gb', function () gitsigns.blame_line{full=true} end, { desc = "Git blame line"})
+vim.keymap.set('n', '<leader>bl', function () gitsigns.blame_line{full=true} end, { desc = "Git blame line"})
