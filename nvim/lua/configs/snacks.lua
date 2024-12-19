3d5a28594ea3af84aaacf44937b429955fdba901
gdebros <guillaume.debros@digeiz.com>
2024-12-19 07:52:35 +0100

    "Removed unused snacks plugins"

--- nvim/lua/configs/snacks.lua
+++ nvim/lua/configs/snacks.lua
@@ -2,12 +2,8 @@ local snacksconfig = {
 
   opts = {
     bigfile = { enabled = true },
-    input = { enabled = false },
-    lazygit = { enabled = false },
-    notifier = { enabled = false },
-    notify = { enabled = false },
+    lazygit = { enabled = true },
     quickfile = { enabled = true },
-    scratch = { enabled = false },
     scroll = { enabled = true },
     zen = { enabled = true },
   },
@@ -15,8 +11,6 @@ local snacksconfig = {
   keys = {
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
     { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
-    { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
-    { "<leader>sb",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
   }
 
 }
