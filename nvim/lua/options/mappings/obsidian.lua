04feafb1b1dd34593ce2c6453083931d8057ed56
gdebros <guillaume.debros@digeiz.com>
2024-12-30 10:11:26 +0100

    "Change obsibian backlink mapping"

--- nvim/lua/options/mappings/obsidian.lua
+++ nvim/lua/options/mappings/obsidian.lua
@@ -2,7 +2,7 @@ vim.keymap.set({ 'n', 'v' }, '<leader>on', ':ObsidianTemplate note<CR>', { desc 
 vim.keymap.set({ 'n', 'v' }, '<leader>ot', ':ObsidianTemplate<CR>', { desc = 'Open Telescope to select template to reformat file from'})
 vim.keymap.set({ 'n', 'v' }, '<leader>of', ':s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<CR>', { desc = 'Obsidian reformat file from note template'})
 vim.keymap.set({ 'n', 'v' }, '<leader>ol', ':ObsidianLinkNew<CR>', { desc = 'Create New Link from selected text'})
-vim.keymap.set({ 'n', 'v' }, '<leader>bl', ':ObsidianBacklinks<CR>', { desc = 'Open Obsidian Backlink in Telescope'})
+vim.keymap.set({ 'n', 'v' }, '<leader>obl', ':ObsidianBacklinks<CR>', { desc = 'Open Obsidian Backlink in Telescope'})
 vim.keymap.set({'n', 'v'}, '<leader>ok', function ()
   require("options.functions.obsidian_organize")()
 end,
