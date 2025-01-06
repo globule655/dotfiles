b171ab58d5cb8416679154265daf8d0573c7f1c8
gdebros <guillaume.debros@digeiz.com>
2025-01-06 09:30:38 +0100

    "Added delete buffer mapping for telescope"

--- nvim/lua/configs/telescope.lua
+++ nvim/lua/configs/telescope.lua
@@ -53,7 +53,11 @@ local telescope_config = {
       -- Developer configurations: Not meant for general override
       buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
       mappings = {
-        n = { ["q"] = require("telescope.actions").close },
+        n = {
+          ["q"] = require("telescope.actions").close,
+          ["<C-d>"] = require('telescope.actions').delete_buffer,
+          ["<C-h>"] = "which_key",
+        },
       },
     },
     extensions_list = { "themes", "terms", "fzf", "ssh-config" },
