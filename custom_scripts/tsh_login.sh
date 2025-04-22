5dd6d4c401d56d71593eab326fde31f3d1b474f1
gdebros <guillaume.debros@digeiz.com>
2025-04-22 10:52:39 +0200

    "Fix login "

--- custom_scripts/tsh_login.sh
+++ custom_scripts/tsh_login.sh
@@ -2,11 +2,11 @@
 
 PROXY_DOMAIN="tlp.digeiz.fr"
 TLP_VAULT_ID="8ae12b3d-84b5-4e8b-b157-eb70842fc01a"
-SESSION_KEY=$(bw login "guillaume.debros@digeiz.com" --raw)
 
 tsh_login() {
-    # Ensure bw is logged out first
+    # Ensure bw is logged out first and get session key
     bw logout
+    SESSION_KEY=$(bw login "guillaume.debros@digeiz.com" --raw)
     # Récupérer le mot de passe et le code TOTP dans des variables
     PASSWORD=$(bw get password $TLP_VAULT_ID --session $SESSION_KEY)
     TOTP=$(bw get totp $TLP_VAULT_ID --session $SESSION_KEY)
