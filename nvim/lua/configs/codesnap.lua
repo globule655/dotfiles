7330f552787b3bb945df7d90e6ac95caea8e4b84
gdebros <guillaume.debros@digeiz.com>
2026-01-27 14:36:19 +0100

    update nvim plugins

--- nvim/lua/configs/codesnap.lua
+++ nvim/lua/configs/codesnap.lua
@@ -1,19 +1,27 @@
 local codesnap_setup = {
   save_path = "~/Pictures",
-  mac_window_bar = false,
-  title = "CodeSnap.nvim",
-  code_font_family = "CaskaydiaCove Nerd Font",
-  watermark_font_family = "Pacifico",
-  watermark = "",
-  bg_theme = "default",
-  breadcrumbs_separator = "/",
-  has_breadcrumbs = false,
-  has_line_number = false,
+  show_line_number = false,
   show_workspace = false,
-  min_width = 0,
-  bg_padding = 0,
-  bg_x_padding = 122,
-  bg_y_padding = 82,
+  snapshot_config = {
+    theme = "default",
+    window = {
+      mac_window_bar = false,
+      margin = {
+        x = 122,
+        y = 82,
+      },
+    },
+    code_config = {
+      font_family = "CaskaydiaCove Nerd Font",
+      breadcrumbs = {
+        enable = false,
+        separator = "/",
+      },
+    },
+    watermark = {
+      content = "",
+      font_family = "Pacifico",
+    },
+  },
 }
-
 return codesnap_setup
